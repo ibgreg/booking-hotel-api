245df249461654638b2c12e267db7633125328e6
Ítalo Gregório <italo-07@hotmail.com>
2022-08-24 19:36:40 -0300

    Added initial Customer, Reservation and Room entity models

---
+++ src/main/java/com/ibgregorio/bookinghotel/entity/Reservation.java
@@ -0,0 +1,85 @@
+package com.ibgregorio.bookinghotel.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Reservation implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
+	
+	@OneToOne
+	@JoinColumn(name = "room_id", referencedColumnName = "roomNumber")
+	private Room room;
+	
+	private Date startDate;
+	
+	private Date endDate;
+
+	public Reservation(Long id, Customer customer, Room room, Date startDate, Date endDate) {
+		super();
+		this.id = id;
+		this.customer = customer;
+		this.room = room;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+}
---
+++ src/main/java/com/ibgregorio/bookinghotel/entity/Room.java
@@ -0,0 +1,101 @@
+package com.ibgregorio.bookinghotel.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Room implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long roomNumber;
+	
+	private Double price;
+	
+	private String category;
+	
+	private Integer numberOfBeds;
+	
+	private boolean balconyAvailable;
+	
+	@OneToOne(mappedBy = "room")
+	private Reservation reservation;
+
+	public Long getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Long roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Integer getNumberOfBeds() {
+		return numberOfBeds;
+	}
+
+	public void setNumberOfBeds(Integer numberOfBeds) {
+		this.numberOfBeds = numberOfBeds;
+	}
+
+	public boolean isBalconyAvailable() {
+		return balconyAvailable;
+	}
+
+	public void setBalconyAvailable(boolean balconyAvailable) {
+		this.balconyAvailable = balconyAvailable;
+	}
+
+	public Reservation getReservation() {
+		return reservation;
+	}
+
+	public void setReservation(Reservation reservation) {
+		this.reservation = reservation;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((roomNumber == null) ? 0 : roomNumber.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Room other = (Room) obj;
+		if (roomNumber == null) {
+			if (other.roomNumber != null)
+				return false;
+		} else if (!roomNumber.equals(other.roomNumber))
+			return false;
+		return true;
+	}
+
+}
