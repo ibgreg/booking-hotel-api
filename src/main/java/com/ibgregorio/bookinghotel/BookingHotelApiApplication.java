6335731659447e0b79799d8f54c420bff05628a5
Ítalo Gregório <italo-07@hotmail.com>
2022-08-23 21:51:27 -0300

    Initial commit

---
+++ src/main/java/com/ibgregorio/bookinghotel/BookingHotelApiApplication.java
@@ -0,0 +1,13 @@
+package com.ibgregorio.bookinghotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHotelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingHotelApiApplication.class, args);
+	}
+
+}
